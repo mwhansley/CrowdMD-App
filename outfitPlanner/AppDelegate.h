7c083d22aac90f0fa7fd9b46f7ee52b58e81e3c4
Megan Hansley <meganwhansley@gmail.com>
2014-08-31 14:12:14 -0700

    Initial Commit

---
+++ outfitPlanner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  outfitPlanner
+//
+//  Created by Megan Hansley on 8/31/14.
+//  Copyright (c) 2014 Simple programmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
